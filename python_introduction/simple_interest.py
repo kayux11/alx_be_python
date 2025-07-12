3e75e3b53af9a87454a2d083ddb0d4bbdf05ebf7
abdulkarim sharafudeen adegboye <abdulkarimsarafadeen@gmail.com>
2025-07-12 12:11:13 +0000

    Calculation of simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Define the variables in the problem
+
+principal = 1000 # respresents $1000
+rate = 0.05 # represents 5% annual interest rate
+time = 3 # represents 3yrs
+
+# Calculate simple interest using the formula I = P * R * T
+Interest = principal * rate * time
+# print the calculated interest using the specified format
+print(f"The simple interest is: {Interest}")
