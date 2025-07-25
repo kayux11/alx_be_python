696dc84badccc6bfe4fff27acebe049385798fe5
abdulkarim sharafudeen adegboye <abdulkarimsarafadeen@gmail.com>
2025-07-25 16:07:55 +0000

    shopping list management

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,19 +1,3 @@
-from arithmetic_operations import perform_operation
-
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
-if __name__ == "__main__":
-    main()
-
-
-
 from shopping_list_manager import display_menu
 def display_menu():
     print("\nShopping List Manager")
